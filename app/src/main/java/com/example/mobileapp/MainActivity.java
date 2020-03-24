c1d177e87bbd043a6659eeab8ec62d9de48660b5
sharoncheeran <sharoncheeran@users.noreply.github.com>
2020-03-24 14:18:14 +0000

    Home screen added

--- app/src/main/java/com/example/mobileapp/MainActivity.java
+++ app/src/main/java/com/example/mobileapp/MainActivity.java
@@ -15,18 +15,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button)findViewById(R.id.createacc);
+        button = (Button)findViewById(R.id.createacc); //identifying the button to be used
         button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v) { //creating the function to be used when clicked
                 openCreateAcc();
+            }
+        });
+
+        button = (Button)findViewById(R.id.signin); //identifying the button to be used
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { //creating the function to be used when clicked
+                openMainpage();
             }
         });
     }
 
-    public void openCreateAcc()
+    public void openCreateAcc() //the page that opens when clicked
     {
         Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+    }
+
+    public void openMainpage() //the page that opens when clicked
+    {
+        Intent intent = new Intent(this, Main3Activity.class);
         startActivity(intent);
     }
 }
